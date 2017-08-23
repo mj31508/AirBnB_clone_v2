d4924c9049b3f10f37a7b3f6bfa66873ef1bc1f9
Max <mj31508@gmail.com>
2017-08-23 16:34:54 -0400

    first commit

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
-# This script configures servers to deploy static content on the web
+# Bash script that sets up web server deployment
+
 sudo apt-get update
+
 sudo apt-get install -y nginx
+
 sudo service nginx start
-sudo mkdir -p /data/web_static/shared/
+
 sudo mkdir -p /data/web_static/releases/test
-echo -e '<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
-sudo chown --recursive ubuntu:ubuntu /data/
-sudo sed -i '38 i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-enabled/default
+
+sudo mkdir -p /data/web_static/releases/shared
+
+echo "<!DOCTYPE html><html><head></head><body> Holberton School html</body></html>" > /data/web_static/releases_test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sed -i '31 i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+
 sudo service nginx restart
