3a940f09e6990451f5ce133ce9fc5a7d84fd77a1
Max <mj31508@gmail.com>
2017-08-23 18:43:18 -0400

    setting up a web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 # Bash script that sets up web server deployment
 
 sudo apt-get update
@@ -11,12 +11,14 @@ sudo mkdir -p /data/web_static/releases/test
 
 sudo mkdir -p /data/web_static/releases/shared
 
-echo "<!DOCTYPE html><html><head></head><body> Holberton School html</body></html>" > /data/web_static/releases_test/index.html
+sudo touch /data/web_static/releases/test/index.html
+
+echo "<!DOCTYPE html><html><head></head><body> Holberton School html</body></html>" > /data/web_static/releases/test/index.html
 
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '31 i \\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '31 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
