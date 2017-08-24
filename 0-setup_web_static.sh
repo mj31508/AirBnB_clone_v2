01760d6fa21c6b6de9e80d906b1b3bea137a7f8f
Max <mj31508@gmail.com>
2017-08-24 00:41:25 -0400

    removal of html tag

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,7 +13,7 @@ sudo mkdir -p /data/web_static/releases/shared
 
 sudo touch /data/web_static/releases/test/index.html
 
-echo "<!DOCTYPE html><html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 
 sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
