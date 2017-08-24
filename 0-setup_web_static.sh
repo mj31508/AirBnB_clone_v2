4482f7bba97feeb20a36bbfcf8e90cfb569e223e
Max <mj31508@gmail.com>
2017-08-24 01:53:06 -0400

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,15 +5,13 @@ sudo apt-get update
 
 sudo apt-get install -y nginx
 
-sudo service nginx start
-
 sudo mkdir -p /data/web_static/shared
 
 sudo mkdir -p /data/web_static/releases/test/
 
-echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
