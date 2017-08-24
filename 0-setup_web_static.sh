be5b41baa8a16fa092d462b91f9ec10c4fa9ff5f
Max <mj31508@gmail.com>
2017-08-24 01:37:57 -0400

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,9 +9,7 @@ sudo service nginx start
 
 sudo mkdir -p /data/web_static/shared
 
-sudo mkdir -p /data/web_static/releases/test
-
-sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
 
 echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 
