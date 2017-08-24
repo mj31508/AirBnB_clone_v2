6e1851f7710f5f6a5a63a8d91029cbd662ac781e
Max <mj31508@gmail.com>
2017-08-23 21:25:40 -0400

    Bash script that sets up web servers for the deployment of a web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,6 +19,6 @@ sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sed -i '31 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '31 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
