37a6496bfeae3c33471d5e81bfdaf2c0cd86439a
Max <mj31508@gmail.com>
2017-08-24 01:16:09 -0400

    removal of lines

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,18 +7,18 @@ sudo apt-get install -y nginx
 
 sudo service nginx start
 
-sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
 
-sudo mkdir -p /data/web_static/releases/shared
+sudo mkdir -p /data/web_static/releases/test
 
 sudo touch /data/web_static/releases/test/index.html
 
 echo "<html><head></head><body>Holberton School</body></html>" > /data/web_static/releases/test/index.html
 
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '39 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed tee -i '39 i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
 sudo service nginx restart
