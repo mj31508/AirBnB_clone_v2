52a6ad3aeb45e3bcdbeec9a78f865a8259e8de0f
Max <mj31508@gmail.com>
2017-08-24 18:04:59 -0400

    fix

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Bash script that sets up web server deployment
+# Bash script that setup web server deployment
 
 sudo apt-get update
 
@@ -9,20 +9,13 @@ sudo mkdir -p /data/web_static/shared
 
 sudo mkdir -p /data/web_static/releases/test
 
-echo -e
-"<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -fs /data/web_static/releases/test /data/web_static/current
 
 sudo chown -R ubuntu:ubuntu /data
 
-sudo sed tee -i '39 i \\tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+sudo sed -i '39 i\   \tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
 
 sudo service nginx start
 sudo service nginx reload
