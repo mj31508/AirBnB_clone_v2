dc20b03180572713327a1ef8615e75687886188f
Max <mj31508@gmail.com>
2017-08-26 18:18:09 -0400

    sending files to tow servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@
 import os
 import time
 from fabric.api import local, hosts, env, run, put
-env.use_ssh_config=True
+env.use_ssh_config = True
 env.hosts = ["66.70.187.58", "54.221.183.295"]
 
 
@@ -14,17 +14,17 @@ def do_deploy(archive_path):
         return False
 
     try:
-        new_arc = archive_path.split("/")[-1]
-        new_dir = ("/data/web_static/releases/" + new_arc.split(".")[0])
-        new_path = "/tmp/" + archive
+        n_arc = archive_path.split("/")[-1]
+        n_dir = ("/data/web_static/releases/" + new_arc.split(".")[0])
+        n_path = "/tmp/" + archive
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p /data/web_static/releass/{}/".format(new_dir))
-        run("sudo tar -xzf /tmp/{} -C /{}/{}/".format(new_arc,new_path,new_dir))
-        run("sudo rm -rf" + new_path)
-        run("sudo mv" + new_arc + "web_static/*" + new_dir)
-        run("sudo rm -rf" + new_dir + "web_static")
+        run("sudo mkdir -p /data/web_static/releass/{}/".format(n_dir))
+        run("sudo tar -xzf /tmp/{} -C /{}/{}/".format(n_arc, n_path, n_dir))
+        run("sudo rm -rf" + n_path)
+        run("sudo mv" + n_arc + "web_static/*" + n_dir)
+        run("sudo rm -rf" + n_dir + "web_static")
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_dir))
+        run("sudo ln -s {} /data/web_static/current".format(n_dir))
         return True
     except:
         return False
