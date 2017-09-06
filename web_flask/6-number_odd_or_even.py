59919a7409070c684f3cd143c6346fe69882c63e
Max <mj31508@gmail.com>
2017-09-06 19:42:56 -0400

    adding a script that checks for odd or even numbers

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import render_template
+"""
+starting the flask application
+"""
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def non_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def ctext(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def default(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route('/number/<int:num>')
+def number(num):
+        return "{} is a number".format(num)
+
+
+@app.route('/number_template/<int:number>')
+def render(number):
+    return render_template('5-number.html', number=number)
+
+
+@app.route('/number_odd_or_even/<int: integer>')
+def o_e(integer):
+    odd_even = "even" if (integer % 2 == 0) else "odd"
+    return render_template("6-number_odd_or_even.html", integer=integer,
+                           o_e=o_e)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
