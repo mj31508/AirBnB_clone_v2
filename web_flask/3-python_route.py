d97a14b5501e2495d93d41e351beae1dd8e6ce42
Max <mj31508@gmail.com>
2017-09-06 13:58:42 -0400

    starting Flask application

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from flask import Flask
+"""
+starting the flask application
+"""
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def non_hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def ctext(text):
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def default(text="is cool"):
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
